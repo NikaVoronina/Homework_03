765268a46e8a676143d146c8a13ca4ac2d5f7ce9
Ника Воронина <nika_voronina_95@mail.ru>
2022-08-17 15:54:21 +0300

    Task added

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,36 +5,20 @@
    
 Console.Write("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(IsPalindrome(num));
-        
-static bool IsPalindrome(int num)
+if (num > 99999 || num < 10000)
 {
-    if (num >= 0 && num <10)
-        return true;
-    int numLength = GetLength(num);
-    int[] digits = new int[numLength];
-    for (int i = numLength - 1; i >= 0; i--) 
-    {
-        digits[i] = num % 10;
-        num /= 10;
-    }
-    for (int i = 0; i < numLength/2; i++) 
-    {
-        if (digits[i] != digits[numLength - i - 1])
-            return false;
-    }
-        return true;
+    Console.Write("Введено не пятизначное число!"); 
+    return;
 }
-        
-static int GetLength(int num)
+if (num / 10000 != num % 10)
 {
-    int n = 0;
-    while (num > 0) 
-    {
-        num /= 10;
-        n++;
-    }
-    return n;
+    Console.Write($"{num} -> не палиндром");
 }
-
-
+else if ((num / 1000) % 10 != (num % 100) / 10)
+{
+    Console.Write($"{num} -> не палиндром");
+}
+else
+{
+    Console.Write($"{num} -> палиндром");
+}
